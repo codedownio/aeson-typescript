8023174a46317fb575df833992dff659cdb8df5e
Tom McLaughlin <tom@codedown.io>
2021-03-25 00:29:01 -0700

    Add Data.Aeson.TypeScript.Internal module

---
+++ src/Data/Aeson/TypeScript/Internal.hs
@@ -0,0 +1,8 @@
+-- | Internal details. For now, this module just exports the full TSDeclaration constructors.
+-- These are subject to breaking changes but are exported here in case you want to live dangerously.
+
+module Data.Aeson.TypeScript.Internal (
+  TSDeclaration(..)
+  ) where
+
+import Data.Aeson.TypeScript.Types
