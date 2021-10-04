6d97475bad1a82e7ff423b97d126a24ecd0e593b
Niklas Hambüchen <mail@nh2.me>
2021-10-04 21:38:07 +0200

    Export `defaultFormattingOptions`.
    No point having a default when the user cannot access to override it!
    
    This was forgotten in PR #22 when extracting it from PR #17.

--- src/Data/Aeson/TypeScript/TH.hs
+++ src/Data/Aeson/TypeScript/TH.hs
@@ -126,6 +126,7 @@ module Data.Aeson.TypeScript.TH (
   , formatTSDeclarations'
   , formatTSDeclaration
   , FormattingOptions(..)
+  , defaultFormattingOptions
   , SumTypeFormat(..)
   , ExportMode(..)
 
