cf3e9495f1199a8452da1594ef76df6ae9e1023b
Sam Protas <samp@mercury.com>
2022-09-23 12:01:05 -0400

    More tests

--- test/OmitFields.hs
+++ test/OmitFields.hs
@@ -18,6 +18,7 @@ module OmitFields (allTests) where
 import Data.Aeson as A
 import Data.Aeson.TypeScript.TH
 import Data.Aeson.TypeScript.Types
+import Data.Char (toLower)
 import Data.Proxy
 import Data.String.Interpolate
 import Prelude hiding (Double)
@@ -27,12 +28,28 @@ import Test.Hspec
 data TwoField = TwoField { doubleInt :: Int, doubleString :: String }
 $(deriveTypeScript' A.defaultOptions ''TwoField defaultExtraTypeScriptOptions {omitFields =["doubleInt"]})
 
+data EmptyTypeScript = EmptyTypeScript { emptyInt :: Int, emptyString :: String }
+$(deriveTypeScript' A.defaultOptions ''EmptyTypeScript defaultExtraTypeScriptOptions {omitFields =["emptyInt", "emptyString"]})
+
+data IgnoreFieldLabelModifier = IgnoreFieldLabelModifier { prefixInt :: Int, prefixString :: String }
+$(deriveTypeScript' A.defaultOptions {A.fieldLabelModifier = map toLower . drop 6} ''IgnoreFieldLabelModifier defaultExtraTypeScriptOptions {omitFields =["int", "prefixString"]})
 
 allTests :: SpecWith ()
 allTests = describe "OmitFields tests" $ do
-    it [i|Removes a field|] $ do
+    it [i|removes a field|] $ do
         (getTypeScriptDeclarations (Proxy :: Proxy TwoField)) `shouldBe` [
             TSTypeAlternatives {typeName = "TwoField", typeGenericVariables = [], alternativeTypes = ["ITwoField"]},
             TSInterfaceDeclaration {interfaceName = "ITwoField", interfaceGenericVariables = [], interfaceMembers = [TSField {fieldOptional = False, fieldName = "doubleString", fieldType = "string"}]}
             ]
 
+    it [i|can remove all fields|] $ do
+        (getTypeScriptDeclarations (Proxy :: Proxy EmptyTypeScript)) `shouldBe` [
+            TSTypeAlternatives {typeName = "EmptyTypeScript", typeGenericVariables = [], alternativeTypes = ["IEmptyTypeScript"]},
+            TSInterfaceDeclaration {interfaceName = "IEmptyTypeScript", interfaceGenericVariables = [], interfaceMembers = []}
+            ]
+
+    it [i|ignores aeson field label modifiers|] $ do
+        (getTypeScriptDeclarations (Proxy :: Proxy IgnoreFieldLabelModifier)) `shouldBe` [
+            TSTypeAlternatives {typeName = "IgnoreFieldLabelModifier", typeGenericVariables = [], alternativeTypes = ["IIgnoreFieldLabelModifier"]},
+            TSInterfaceDeclaration {interfaceName = "IIgnoreFieldLabelModifier", interfaceGenericVariables = [], interfaceMembers = [TSField {fieldOptional = False, fieldName = "int", fieldType = "number"}]}
+            ]
