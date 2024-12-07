c659b5b3f85e5b1e08d5b303221ecb53cb85ffff
thomasjm <tom@codedown.io>
2024-12-06 16:04:45 -0800

    Add flake.nix, .envrc

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "aeson-typescript";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+  inputs.nixpkgsMaster.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, nixpkgsMaster, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pkgsMaster = import nixpkgsMaster { inherit system; };
+      in
+        {
+          packages = {
+            inherit (pkgsMaster.haskell.packages.ghc966) weeder;
+
+            test = pkgs.writeShellScriptBin "stack-test" ''
+              export NIX_PATH=nixpkgs=${pkgs.path}
+              ${pkgs.stack}/bin/stack test
+            '';
+
+            nixpkgsPath = pkgs.writeShellScriptBin "nixpkgsPath.sh" "echo -n ${pkgs.path}";
+          };
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs
+            ];
+          };
+        });
+}
